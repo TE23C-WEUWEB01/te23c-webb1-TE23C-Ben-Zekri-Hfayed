1a26d9f918e4a3e21e40de210139940398be6a99
Hfayed Ben Zekri <hfayed.benzekri@elev.ga.ntig.se>
2025-03-13 14:25:20 +0100

    tränat på JS

---
+++ kapitel-8/introjs/script.js
@@ -0,0 +1,16 @@
+// var tal1 = 5;
+// var tal2 = 2;
+// var summa = tal1 + tal2
+// alert(tal1 + " + " + tal2 + " = " + summa );
+
+var förnamn = "Shawarma";
+var efternamn = "Borkan";
+var ålder = 31;
+var namn = förnamn + " " + efternamn;
+// var message = namn + " är " +  ålder + " år gammal. ";
+// alert(message);
+
+var hobby = "segling";
+var kostnad = 18000 + "kr";
+var message = namn + " gillar " + hobby + "\n och det kostar " + kostnad + " per år";
+alert(message);
