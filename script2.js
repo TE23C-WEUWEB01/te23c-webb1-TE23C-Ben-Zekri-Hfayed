21354d2d67a1df1df81817af06f45ad2c79bc0d5
Hfayed Ben Zekri <hfayed.benzekri@elev.ga.ntig.se>
2025-03-28 09:41:29 +0100

    ddadsdadasda

---
+++ script2.js
@@ -0,0 +1,21 @@
+// alert("Glädjens dag, det är fredag!");
+// var namn = prompt("Vad heter du?");
+// var ålder = prompt("Hur gammal är du?");
+// alert(" Hej " + namn + " du är " + ålder + " år gammal ");
+// console.log("Det verkar att det funkar");
+
+//be användare att mata in 2 tal och skriv summan
+
+//  tal1 = prompt("Skriv första talet")
+// var tal2 = prompt("Skriv andra talet");
+// var summan  = parseInt(tal1) + parseInt(tal2)
+ 
+// alert(" Du skrev " + tal1 + " + " + tal2 + "," + " " + " det blir " + summan)
+
+// var ålder = prompt("Hur gammal är du?")
+// var student = ("18")
+// var summan = parseInt(student) - parseInt(ålder)
+// document.writeln("<h2> Du har " + summan + " år " + "kvar</h2> ")
+
+var slumptal1 = Math.floor(Math.random() * 100)
+console.log(slumptal1);
